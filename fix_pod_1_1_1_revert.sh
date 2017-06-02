f8d660a82adc151e90280a350a91f3b61a2edffd
Dacaiguoguo <dacaiguoguo@gmail.com>
2017-06-02 12:14:53 +0800

    Create fix_pod_1_1_1_revert.sh

---
+++ fix_pod_1_1_1_revert.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+podpath=$(echo `which pod`)
+poddir=$(dirname $podpath)
+podcoregemdir="$poddir"/../gems/cocoapods-core-1.1.1/lib/cocoapods-core/
+echo "$podcoregemdir"lockfile.rb
+cd $podcoregemdir
+curl -fsSL https://raw.githubusercontent.com/CocoaPods/Core/1.1.1/lib/cocoapods-core/lockfile.rb -O
