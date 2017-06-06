35b2e115dee231a0fcf62e20ea1731d54dddad71
dacaiguoguo <dacaiguoguo@gmail.com>
2017-06-06 14:51:26 +0800

    Update fix_pod_1_1_1.sh

--- fix_pod_1_1_1.sh
+++ fix_pod_1_1_1.sh
@@ -3,9 +3,16 @@
 poddir=$(echo `gem env gemdir`)
 podcoregemdir="$poddir"/gems/cocoapods-core-1.1.1/lib/cocoapods-core/
 cd $podcoregemdir
+if [ $? -eq 0 ];then
+    echo "cd success"
+  else
+    echo "cd fail"
+    exit 1
+fi
 if [[ $poddir = "/Library/Ruby/Gems/2.0.0" ]]; then
     sudo curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/Core/master/lib/cocoapods-core/lockfile.rb -O
 else
     curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/Core/master/lib/cocoapods-core/lockfile.rb -O
 fi
 echo "$podcoregemdir"lockfile.rb
+echo "fix success"
