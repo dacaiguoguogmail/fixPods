bb42bff03024f007d666229a94105c34911071b2
dacaiguoguo <dacaiguoguo@gmail.com>
2017-06-06 20:34:31 +0800

    Update fix_pod_1_1_1.sh

--- fix_pod_1_1_1.sh
+++ fix_pod_1_1_1.sh
@@ -1,35 +1,10 @@
 #!/bin/zsh
 
 poddir=$(echo `gem env gemdir`)
-podcoregemdir="$poddir"/gems/cocoapods-core-1.1.1/lib/cocoapods-core/
-cd $podcoregemdir
-if [ $? -eq 0 ];then
-    echo "cd core success"
-  else
-    echo "cd core fail"
-    exit 1
-fi
 if [[ $poddir = "/Library/Ruby/Gems/2.0.0" ]]; then
-    sudo curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/Core/master/lib/cocoapods-core/lockfile.rb -O
-else
-    curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/Core/master/lib/cocoapods-core/lockfile.rb -O
+    sudo -s
 fi
-echo "$podcoregemdir"lockfile.rb
-cocoapodsipcgemdir="$poddir"/gems/cocoapods-1.1.1/lib/cocoapods/command/ipc/
-cd $cocoapodsipcgemdir
-if [ $? -eq 0 ];then
-    echo "cd ipc success"
-  else
-    echo "cd ipc fail"
-    exit 1
-fi
-if [[ $poddir = "/Library/Ruby/Gems/2.0.0" ]]; then
-    sudo curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/CocoaPods/master/lib/cocoapods/command/ipc/podfile_json.rb -O
-    cd "$poddir"/gems/cocoapods-1.1.1/lib/cocoapods/command/
-    sudo curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/CocoaPods/master/lib/cocoapods/command/ipc.rb -O
-else
-    curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/CocoaPods/master/lib/cocoapods/command/ipc/podfile_json.rb -O
-    curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/CocoaPods/master/lib/cocoapods/command/ipc.rb -O
-fi
-echo "podfile_json.rb did add"
+curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/Core/master/lib/cocoapods-core/lockfile.rb -o "$poddir"/gems/cocoapods-core-1.1.1/lib/cocoapods-core/
+curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/CocoaPods/master/lib/cocoapods/command/ipc/podfile_json.rb -o "$poddir"/gems/cocoapods-1.1.1/lib/cocoapods/command/ipc/
+curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/CocoaPods/master/lib/cocoapods/command/ipc.rb -o "$poddir"/gems/cocoapods-1.1.1/lib/cocoapods/command/
 echo "fix success"
