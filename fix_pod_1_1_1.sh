9fb4dcff603e70c8c55e1374808e17d0e63790ce
dacaiguoguo <dacaiguoguo@gmail.com>
2017-06-06 20:18:35 +0800

    Update fix_pod_1_1_1.sh
    podfile_json.rb add

--- fix_pod_1_1_1.sh
+++ fix_pod_1_1_1.sh
@@ -4,9 +4,9 @@ poddir=$(echo `gem env gemdir`)
 podcoregemdir="$poddir"/gems/cocoapods-core-1.1.1/lib/cocoapods-core/
 cd $podcoregemdir
 if [ $? -eq 0 ];then
-    echo "cd success"
+    echo "cd core success"
   else
-    echo "cd fail"
+    echo "cd core fail"
     exit 1
 fi
 if [[ $poddir = "/Library/Ruby/Gems/2.0.0" ]]; then
@@ -15,4 +15,18 @@ else
     curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/Core/master/lib/cocoapods-core/lockfile.rb -O
 fi
 echo "$podcoregemdir"lockfile.rb
+cocoapodsipcgemdir="$poddir"/gems/cocoapods-1.1.1/lib/cocoapods/command/ipc/
+cd $cocoapodsipcgemdir
+if [ $? -eq 0 ];then
+    echo "cd ipc success"
+  else
+    echo "cd ipc fail"
+    exit 1
+fi
+if [[ $poddir = "/Library/Ruby/Gems/2.0.0" ]]; then
+    sudo curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/CocoaPods/master/lib/cocoapods/command/ipc/podfile_json.rb -O
+else
+    curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/CocoaPods/master/lib/cocoapods/command/ipc/podfile_json.rb -O
+fi
+echo "podfile_json.rb did add"
 echo "fix success"
