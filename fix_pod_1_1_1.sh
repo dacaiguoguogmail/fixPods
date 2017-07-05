6406866e2656033b2f56cdc3cd182c5e7a0b41d0
sunyanguo <sunyanguo@lvmama.com>
2017-07-05 14:50:39 +0800

    fix uuid warning

--- fix_pod_1_1_1.sh
+++ fix_pod_1_1_1.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 poddir=$(echo `gem env gemdir`)
+uuid_gen_path=$(echo `gem env gemdir`/gems/xcodeproj-`xcodeproj --version `/lib/xcodeproj/project/uuid_generator.rb)
 if [[ $poddir = "/Library/Ruby/Gems/2.0.0" ]]; then
 sudo curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/Core/master/lib/cocoapods-core/lockfile.rb -o "$poddir"/gems/cocoapods-core-1.1.1/lib/cocoapods-core/lockfile.rb
 echo "$poddir"/gems/cocoapods-core-1.1.1/lib/cocoapods-core/lockfile.rb
@@ -9,6 +10,8 @@ echo "$poddir"/gems/cocoapods-1.1.1/lib/cocoapods/command/ipc/podfile_json.rb
 sudo curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/CocoaPods/master/lib/cocoapods/command/ipc.rb -o "$poddir"/gems/cocoapods-1.1.1/lib/cocoapods/command/ipc.rb
 echo "$poddir"/gems/cocoapods-1.1.1/lib/cocoapods/command/ipc.rb
 echo -e "\033[32mfix success\033[0m"
+sudo curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/fixPods/master/uuid_generator.rb -o $uuid_gen_path
+echo $uuid_gen_path
 exit 0
 fi
 curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/Core/master/lib/cocoapods-core/lockfile.rb -o "$poddir"/gems/cocoapods-core-1.1.1/lib/cocoapods-core/lockfile.rb
@@ -17,4 +20,10 @@ curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/CocoaPods/master/l
 echo "$poddir"/gems/cocoapods-1.1.1/lib/cocoapods/command/ipc/podfile_json.rb
 curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/CocoaPods/master/lib/cocoapods/command/ipc.rb -o "$poddir"/gems/cocoapods-1.1.1/lib/cocoapods/command/ipc.rb
 echo "$poddir"/gems/cocoapods-1.1.1/lib/cocoapods/command/ipc.rb
+curl -fsSL https://raw.githubusercontent.com/dacaiguoguogmail/fixPods/master/uuid_generator.rb -o $uuid_gen_path
+echo $uuid_gen_path
 echo -e "\033[32mfix success\033[0m"
+
+
+
+
